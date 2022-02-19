c5fb283b3d6e3a74524d8e546c15bbf5bd5e7b76
imoto-hirofumi <i.hirofumi724@gmail.com>
2022-02-20 00:11:42 +0900

    Initial Commit

---
+++ StartingMakingLoop/StartingMakingLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingMakingLoopApp.swift
+//  StartingMakingLoop
+//
+//  Created by 井本博文 on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct StartingMakingLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
